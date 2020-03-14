1aee68bbf1b64ffe030e871b5d2d772836022003
Ignacio Ochoa <ignacio.ochoa.unicen@gmail.com>
2020-03-14 00:40:46 -0300

    se implemento interfaz de login

---
+++ js/login.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+"use strict";
+
+let Usuarios = {
+
+    "sebita": {
+
+        "login": "sebeishon",
+        "password": "seba102"
+    },
+    "guido": {
+
+        "login": "guidomaster",
+        "password": "diamante1"
+    }, 
+    "nacho": {
+
+        "login": "nacho",
+        "password": "mermelada"
+    }
+}
+
+
+
+});
